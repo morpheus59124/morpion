6f4d4cc8a68961d6163db434f2770241b66ca219
morpheus59124 <crypto.ramberic@gmail.com>
2022-09-22 15:45:41 +0200

    a

--- grille.py
+++ grille.py
@@ -1,27 +1,69 @@
-# definition des joueurs
-joueur1 = str(input("entrez le prenom du joueur 1 : "))
-joueur2 = str(input("entrez le prenom du joueur 2 : "))
-
 # definition de la grille
 grille= [
     [" "," "," "], 
     [" "," "," "],
     [" "," "," "]
 ]
-def affichage ():
+
+# fonctions
+def affichage (grille):
     for i in range(3):
         print(grille[i])
-affichage()
+affichage(grille)
 
-# definition des touches
-touch= {
-    1:"A1",
-    2:"A2",
-    3:"A3",
-    4:"B1",
-    5:"B2",
-    6:"B3",
-    7:"C1",
-    8:"C2",
-    9:"C3"
-}
+# coup gagnant
+def est_gagnant(grille):
+    grille[0]
+    for ligne in grille :
+        if ligne[0]==ligne[1]==ligne[2] and ligne[0]!=" ":
+            return True
+    for i in range(3):
+        if grille[0][i]==grille[1][i]==grille[2][i] and grille[0][i]!=" ":
+            return True
+
+
+
+# definitions des tours
+def tour(grille, joueur):
+    print("C'est le tour du joueur "+str(joueur))
+    ligne=input("Entrez le numero de la ligne : ")
+    colonne=input("Entrez le numero de la colonne : ")
+    print("Vous avez joué la case ("+ligne+","+colonne+")")
+    while grille[int(ligne)][int(colonne)]!= " ":
+        affichage(grille)
+        print("Cette case est deja jouée ! Saisissez une autre case svp !")
+        ligne=input("Entrez le numero de la ligne : ")
+        colonne=input("Entrez le numero de la colonne : ")
+        print("Vous avez joué la case ("+ligne+","+colonne+")")
+        return
+    if joueur==1 :
+        grille[int(ligne)][int(colonne)]="X"
+    if joueur==2 :
+        grille[int(ligne)][int(colonne)]="O"
+    affichage(grille)
+
+# match nul
+def est_match_nul(grille):
+    for i in range(3):
+        if grille[i]==" ":
+            return print("t es nul")
+    return 
+
+#tour gagnant ou termine
+joueur=1
+print("Le joueur 1 possède les X. Le joueur 2 possède les O")
+gagne=0
+while gagne==0:
+    tour(grille,joueur)
+    if est_gagnant(grille):
+        print("Le joueur "+str(joueur)+" remporte la partie")
+        gagne=1
+    else:
+        if est_match_nul(grille):
+            print("Plus de place ! Match nul !")
+            gagne=1
+    if joueur==1:
+        joueur=2
+    else:
+        joueur=1
+
