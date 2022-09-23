6d9904c8a72f7d01fd673059af928be962ec1239
morpheus59124 <crypto.ramberic@gmail.com>
2022-09-23 16:51:59 +0200

    terminer

--- cesar.py
+++ cesar.py
@@ -10,11 +10,6 @@ def freq(texte):
                 
         return d
 
-def dechiffrer(resultat,index2):
-        for lettre in resultat:
-                resultat+=chr(ord(lettre)-index2)
-        return resultat
-
 while True:
     # On récupère les données.
     demande= int(input("voulez vous Chiffrer(1) ou Dechiffrer(2) ou Decrypter(3): "))
@@ -54,7 +49,7 @@ while True:
             print("déchiffrage reussi :)")
             break        
     elif demande==3:
-        texte = result
+        texte = resultat
         fichier = open("texte.txt", "w")        #Créer le fichier s'il n'existe pas
         fichier.write(resultat)        #Écrit la valeur de la variable a dans le fichier
         fichier.close()
@@ -64,17 +59,27 @@ while True:
                 
         L = sorted(freq(texte).items(), key = lambda colonne: colonne[1] , reverse = True)
         for i in L:
-                print('{} : {}'.format(i[0],i[1]))
-        print(L[0][0])
-        index=ord(L[0][0])     
-        index2=index-32
-        print(index2)
-        decalage = index2
-
-        dechiffrer()
-        print(resultat)        
-        print("déchiffrage reussi :)")
-        break        
+                index=ord(L[0][0])     
+                index2=index-32
+                print(index2)
+                decalage = index2
+        phrase = resultat
+        decalage = index2  # Conversion direct en int!
+        dico = {} # On crée un dictionnaire {lettre_de_départ: lettre_encodée}.
+        for i in range(255):  # Pour chaque nombre de 0 à 255
+                i_caesar = (i - index2) % 255 # Modulo 255, pour toujours avoir un résultat compris entre 0 et 25 inclus (modulo 26 = reste de la division euclidienne par 26).
+                # On ajoute à ces nombre la "valeur" de la lettre a, et on les convertit en lettres!
+                c_caesar = chr(i_caesar + 0)
+                c = chr(i + 0)
+                dico[c] = c_caesar
+        # On a maintenant un dictionnaire qui à chaque lettre fait correspondre sa valeur codée.
+        for c in phrase:  # Pour chaque caractère de phrase...
+                resultat = result + dico[c]
+                result=resultat
+        print(resultat)
+        print("décrytage reussi :)")
+        break
+        
                 
         continue
         fichier = remove("texte.txt")  
